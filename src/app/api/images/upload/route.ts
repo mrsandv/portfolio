881ce0bc09b12288ade0389f841c35cb85c1d357
mrsan <mrsandv@pm.me>
2025-10-29 09:16:44 -0600

    add token to blob methods

--- src/app/api/images/upload/route.ts
+++ src/app/api/images/upload/route.ts
@@ -31,6 +31,7 @@ export async function POST(request: Request) {
 		const blob = await put(filename, request.body, {
 			access: 'public',
 			addRandomSuffix: true,
+			token: process.env.BLOB_READ_WRITE_TOKEN,
 		});
 
 		const data = await ImageModel.create({
